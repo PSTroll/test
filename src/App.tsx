5df7fb6a5517d7a201c529d34f15fccce72b0617
PSTroll <pstroll257@gmail.com>
2023-07-01 12:32:29 +0200

    project was created and tailwind added

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-slate-800 h-screen">
+      <div className="text-white">TEST</div>
+    </div>
+  );
+}
+
+export default App;
